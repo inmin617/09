0d21ae61852eeb69b2b10b48fbf5fb3f3e101ab4
inmin617 <144991175+inmin617@users.noreply.github.com>
2023-11-02 21:55:28 +0900

    실습5

--- main.c
+++ main.c
@@ -1,33 +1,50 @@
 #include <stdio.h>
-#define SIZE 4
+#define ROWS 3
+#define COLS 3
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void square_array(int a[], int size)
+void addMatrix(int A[ROWS][COLS], int B[ROWS][COLS], int C[ROWS][COLS])
 {
-	int i;
+	int i, j;
 	
-	for(i=0;i<size;i++)
-		a[i]=a[i]*a[i];
+    for (i=0; i<ROWS; i++)
+	{
+        for (j=0; j<COLS; j++) 
+		{
+            C[i][j] = A[i][j] + B[i][j];
+        }
+    }
 }
 
-void print_array(int a[], int size)
+void printMatrix(int C[ROWS][COLS])
 {
-	int i;
-	
-	for(i=0;i<size;i++)
-		printf("%3d", a[i]);
-	printf("\n");
+	int i, j;
+	 for (i=0; i<ROWS; i++) {
+        for (j=0; j<COLS; j++) {
+            printf("%d ", C[i][j]);
+        }
+        printf("\n"); // 한 행렬이 출력되면 개행 문자를 출력하여 다음 줄로 넘어갑니다.
+    }
 }
 
 int main(void)
 {
+	int A[ROWS][COLS]= { // 더하기 대상 행렬 
+		{2, 3, 0},
+		{8, 9, 1},
+		{7, 0, 5}};
+		
+	int B[ROWS][COLS]={ // 더하기 대상 행렬 
+		{1, 0, 0},
+		{0, 1, 0},
+		{0, 0, 1}};
 	
-	int list[SIZE]={1, 2, 3, 4};
+	int C[ROWS][COLS]; // 덧셈 결과 행렬 
+
 	
-	print_array(list, SIZE);
-	square_array(list, SIZE);
-	print_array(list, SIZE);
+	addMatrix(A, B, C);
+	printMatrix(C) ;
 	
 	return 0;
 }
