e415ec24675d6f310d3d03bcd2c6f45ff6664507
inmin617 <144991175+inmin617@users.noreply.github.com>
2023-11-02 21:08:52 +0900

    실습3_2

--- main.c
+++ main.c
@@ -7,17 +7,20 @@
 int main(int argc, char *argv[]) {
 	
 	int i;
-	int grade[SIZE];
-	int score[SIZE];
-
+	int a[SIZE]={1, 2, 3, 4, 5};
+	int b[SIZE]={1, 2, 3, 4, 5};
+	int flag=0;
+	
 	for(i=0;i<SIZE;i++)
-		grade[i]=rand()%100+1;
-		
-	for(i=0;i<SIZE;i++)
-		score[i]=grade[i];
-		
-	for (i=0; i<SIZE; i++)
-		printf("score[%d]=%d\n", i, score[i]);
+	{
+		if (a[i]!=b[i])
+		{
+			printf("array a and b are not the same\n");
+			flag=1;
+		}
+	}
 		 
+	if (flag==0)
+		printf("array a and b are the same\n");
 	return 0;
 }
